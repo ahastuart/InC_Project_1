743a72fdd9f248c94fa249e2f6682908637948a0
hs-1771304-kukseungbin <86177125+hs-1771304-kukseungbin@users.noreply.github.com>
2024-11-04 11:42:43 +0900

    테스트

--- product.py
+++ product.py
@@ -73,9 +73,9 @@ def generateImageFromPrompt():
             return redirect(url_for('product.createImage'))
         
         # 크레딧 감소 시도
-        if not productDAO().generate_image(user_id):
-            flash('크레딧이 부족하여 이미지를 생성할 수 없습니다.')
-            return redirect(url_for('product.createImage'))
+        # if not productDAO().generate_image(user_id):
+        #     flash('크레딧이 부족하여 이미지를 생성할 수 없습니다.')
+        #     return redirect(url_for('product.createImage'))
 
         dalle_propt = generate_dalle_prompt(user_prompt)
         
@@ -107,10 +107,10 @@ def generateImageFromCategory():
             flash('프롬프트를 입력해주세요')
             return redirect(url_for('product.createImage'))
         
-        # 크레딧 감소 시도
-        if not productDAO().generate_image(user_id):
-            flash('크레딧이 부족하여 이미지를 생성할 수 없습니다.')
-            return redirect(url_for('product.createImage'))
+        # # 크레딧 감소 시도
+        # if not productDAO().generate_image(user_id):
+        #     flash('크레딧이 부족하여 이미지를 생성할 수 없습니다.')
+        #     return redirect(url_for('product.createImage'))
         
         prompt = generate_dalle_category(category, topic)
         
